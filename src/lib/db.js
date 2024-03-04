07e40f94469763e9380ffd74d6a6bf7272e2b619
Abdullah Mughal <itsalphasynapse@gmail.com>
2024-03-04 22:24:47 +0500

    Edited env variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,10 @@
 import { Client } from 'pg';
 
 const {
-    POSTGREAQL_SERVER_USER,
-    POSTGREAQL_SERVER_PASSWORD,
-    POSTGREAQL_SERVER_HOST,
-    POSTGREAQL_SERVER_PORT,
-    DB_NAME
+    POSTGREAQL_USER,
+    POSTGREAQL_PASSWORD,
+    POSTGREAQL_HOST,
+    POSTGREAQL_DATABASE
 } = process.env;
 
 let catchedConnection = null;
@@ -17,30 +16,30 @@ const connectToDataBase = async () => {
     }
     try {
         const postgreSQL = new Client({
-            user: POSTGREAQL_SERVER_USER,
-            password: POSTGREAQL_SERVER_PASSWORD,
-            host: POSTGREAQL_SERVER_HOST,
-            port: POSTGREAQL_SERVER_PORT,
+            user: POSTGREAQL_USER,
+            password: POSTGREAQL_PASSWORD,
+            host: POSTGREAQL_HOST,
+            port: 5432,
         });
 
         await postgreSQL.connect()
 
         const checkResult = await postgreSQL.query(`
         SELECT datname FROM pg_database WHERE datname = $1;
-        `, [DB_NAME]);
+        `, [POSTGREAQL_DATABASE]);
 
         if (checkResult.rows.length === 0) {
-            await postgreSQL.query(`CREATE DATABASE ${DB_NAME};`);
+            await postgreSQL.query(`CREATE DATABASE ${POSTGREAQL_DATABASE};`);
         }
 
         await postgreSQL.end();
 
         const client = new Client({
-            user: POSTGREAQL_SERVER_USER,
-            password: POSTGREAQL_SERVER_PASSWORD,
-            host: POSTGREAQL_SERVER_HOST,
-            database: DB_NAME,
-            port: POSTGREAQL_SERVER_PORT,
+            user: POSTGREAQL_USER,
+            password: POSTGREAQL_PASSWORD,
+            host: POSTGREAQL_HOST,
+            database: POSTGREAQL_DATABASE,
+            port: 5432,
         });
 
         await client.connect();
